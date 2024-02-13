ecba48b64b3477699375251b96fe72dc2b369c90
nk-on <2nkoz@proton.me>
2024-02-13 20:56:00 +0400

    feat:displaying calculated number of years, months and days in the display

--- src/script.js
+++ src/script.js
@@ -12,21 +12,28 @@ The date is invalid, e.g. 31/04/1991 (there are 30 days in April) */
 const yearInput = document.querySelector("#Year-input");
 const monthInput = document.querySelector("#Month-input");
 const dayInput = document.querySelector("#Day-input");
-const yearContainer = document.querySelector("[Year]");
-const monthContainer = document.querySelector("[Month]");
-const dayContainer = document.querySelector("[Day]");
+const yearContainer = document.querySelector("[data-number=Year]");
+const monthContainer = document.querySelector("[data-number=Month]");
+const dayContainer = document.querySelector("[data-number=Day]");
 const submitIcon = document.querySelector(".submit-icon");
 function calculateAge(){
     const currentYear = new Date().getFullYear();
     const currentMonth = new Date().getMonth();
-    const currentDay = getDayOfYear(new Date());                                                                                                                                           
+    const currentDay = getDayOfYear(new Date());
+    const year = currentYear - yearInput.value;
+    displayAge(year,currentMonth,currentDay);                                                                                                                        
 };
+function displayAge(year,month,day){
+    yearContainer.textContent = year;
+    monthContainer.textContent = month;
+    dayContainer.textContent = day;    
+}
 function getDayOfYear(date) {
     const start = new Date(date.getFullYear(), 0, 0);
     const diff = (date - start) + ((start.getTimezoneOffset() - date.getTimezoneOffset()) * 60 * 1000);
     const oneDay = 1000 * 60 * 60 * 24;
     const dayOfYear = Math.floor(diff / oneDay);
     return dayOfYear;
-}
+};
 
 submitIcon.addEventListener("click",calculateAge)

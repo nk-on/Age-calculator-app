f4b253f63d24cda5987ddb8d4b0a93515d3de9af
nk-on <2nkoz@proton.me>
2024-02-13 20:34:36 +0400

    feat:calculatng current year, number of current month and number of current day in the year

--- src/script.js
+++ src/script.js
@@ -1,4 +1,8 @@
 /*View an age in years, months, and days after submitting a valid date through the form
+ //get current year/month/day
+ //subtract age year entered by user to current year
+ //display directly current month  
+ //display result 
 Receive validation errors if:
 Any field is empty when the form is submitted
 The day number is not between 1-31
@@ -11,4 +15,18 @@ const dayInput = document.querySelector("#Day-input");
 const yearContainer = document.querySelector("[Year]");
 const monthContainer = document.querySelector("[Month]");
 const dayContainer = document.querySelector("[Day]");
-const submitIcon = document.querySelector(".submitIcon");
+const submitIcon = document.querySelector(".submit-icon");
+function calculateAge(){
+    const currentYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth();
+    const currentDay = getDayOfYear(new Date());                                                                                                                                           
+};
+function getDayOfYear(date) {
+    const start = new Date(date.getFullYear(), 0, 0);
+    const diff = (date - start) + ((start.getTimezoneOffset() - date.getTimezoneOffset()) * 60 * 1000);
+    const oneDay = 1000 * 60 * 60 * 24;
+    const dayOfYear = Math.floor(diff / oneDay);
+    return dayOfYear;
+}
+
+submitIcon.addEventListener("click",calculateAge)

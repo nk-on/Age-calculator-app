3d2f8a784f0f8d93188d371f283741a23e3c2095
nk-on <2nkoz@proton.me>
2024-02-13 22:33:37 +0400

    subtracting one year to passed years from entered date to current date if entered month has not arrived yet

--- src/script.js
+++ src/script.js
@@ -1,9 +1,5 @@
 /*View an age in years, months, and days after submitting a valid date through the form
- //get current year/month/day
- //subtract age year entered by user to current year
- //display directly current month  
- //display result 
-Receive validation errors if:
+ 
 Any field is empty when the form is submitted
 The day number is not between 1-31
 The month number is not between 1-12
@@ -17,10 +13,13 @@ const monthContainer = document.querySelector("[data-number=Month]");
 const dayContainer = document.querySelector("[data-number=Day]");
 const submitIcon = document.querySelector(".submit-icon");
 function calculateAge(){
-    const currentMonth = (new Date().getMonth() + 1)
-    const yearsPassed = new Date().getFullYear() - yearInput.value;
-    const monthsPassed = ((monthInput.value - currentMonth) + 12*yearsPassed)%12;
+    const currentMonth = (new Date().getMonth() + 1);
+    let yearsPassed = new Date().getFullYear() - yearInput.value;
+    const monthsPassed = 12 - ((monthInput.value - currentMonth) + 12*yearsPassed)%12;
     const daysPassed = new Date().getDate();
+    if(monthInput.value > currentMonth){
+        yearsPassed--;
+    };
     displayAge(yearsPassed,monthsPassed,daysPassed);                                                                                                                        
 };
 function displayAge(year,month,day){
